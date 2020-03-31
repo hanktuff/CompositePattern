942c45957b26fd61fec0a3c42c51914ad272e277
Alen Milakovic <hanktuff@hotmail.com>
2020-03-31 13:01:10 -0700

    added subsubroot

--- CompositeDesignPattern/Program.cs
+++ CompositeDesignPattern/Program.cs
@@ -8,15 +8,21 @@ namespace CompositeDesignPattern {
     class Program {
         static void Main(string[] args) {
 
-            var root = new Composite();
+            var root = new Composite("root");
 
-            root.Leaves.Add(new Leaf());
-            root.Leaves.Add(new Leaf());
+            root.Leaves.Add(new Leaf("leaf 1"));
+            root.Leaves.Add(new Leaf("leaf 2"));
 
-            var subroot = new Composite();
+            var subroot = new Composite("subroot");
+            subroot.Leaves.Add(new Leaf("leaf 3"));
+
             root.Leaves.Add(subroot);
 
-            subroot.Leaves.Add(new Leaf());
+            var subsubroot = new Composite("sub-subroot");
+            subsubroot.Leaves.Add(new Leaf("leaf 4"));
+            subsubroot.Leaves.Add(new Leaf("leaf 5"));
+
+            subroot.Leaves.Add(subsubroot);
 
             root.Operation();
         }
@@ -27,19 +33,28 @@ namespace CompositeDesignPattern {
         public abstract void Operation();
     }
 
+
     class Composite : Component {
+
+        private string name = string.Empty;
+        public Composite(string Name) { name = Name; }
+
         public List<Component> Leaves = new List<Component>();
+
         public override void Operation() {
-            Console.WriteLine("Operation() in Composite class.");
+            Console.WriteLine($"Operation() in Composite class {name}.");
             foreach (var item in this.Leaves) {
                 item.Operation();
             }
         }
     }
 
+
     class Leaf : Component {
-        public override void Operation() {
-            Console.WriteLine("Operation() in Leaf class.");
-        }
+
+        private string name = string.Empty;
+        public Leaf(string Name) { name = Name; }
+
+        public override void Operation() { Console.WriteLine($"Operation() in Leaf class {name}."); }
     }
 }
